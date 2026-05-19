bd9c39e442d23f9c58178bdd4c1e9e1b2b7993da
SUDEV MAJHI <58890958+Sudev0707@users.noreply.github.com>
2026-05-19 15:04:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function toCase(text) {
-  // write your code here
+  if (text.length === 0) return "-";
+  return text.toLowerCase() + "-" + text.toUpperCase();
 }
 
 // DO not change the code below
